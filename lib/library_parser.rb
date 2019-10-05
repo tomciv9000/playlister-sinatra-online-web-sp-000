4052237969a981d43b840f4e8721f3d85a31a82c
Tom White <eugene.white@gmail.com>
2019-10-05 18:05:52 +0000

    Done.

--- lib/library_parser.rb
+++ lib/library_parser.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class LibraryParser
   def files
     data_path = File.join(File.dirname(__FILE__), '..', 'db', 'data')
@@ -5,6 +7,7 @@ class LibraryParser
   end
 
   def self.parse
+    binding.pry
     self.new.call
   end
 
@@ -34,7 +37,7 @@ class LibraryParser
 
     song.song_genres.build(genre: genre)
     song.artist = artist
-    
+
     song.save
   end
 end

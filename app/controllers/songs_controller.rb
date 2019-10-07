4f791e713f1e981e8e9221c871f9d4ccaddf3a77
Tom White <eugene.white@gmail.com>
2019-10-07 15:11:35 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,7 +20,7 @@ class SongsController < ApplicationController
      @song.genre_ids = params[:genres]
      @song.save
 
-     erb :'songs/show', locals: {message: "Successfully created song."}
+     redirect "/songs/#{@song.slug}", locals: {message: "Successfully created song."}
    end
  #
  #   get '/songs/:slug/edit' do
